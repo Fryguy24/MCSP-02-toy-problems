63f5a3c0ab757dfdeb028dc2cec6b0546ce34c92
Julius Fry <fryjulius@gmail.com>
2019-12-17 09:54:23 -0600

    done with isBalanced

--- balancedBrackets/prompt.js
+++ balancedBrackets/prompt.js
@@ -9,4 +9,40 @@
 // str: "[{()}]"                  |   true
 // str: "(50)("                   |   false
 // str: "[{]}"                    |   false
-const isBalanced = str => {};
+const isBalanced = str => {
+    let array = [];
+    let correspond = {
+        // Making sure each opening brace is the same as the corresponding closing brace.
+        '(': ')',
+        '[': ']',
+        '{': '}'
+    }
+    // There is a function that takes in a parameter, which is a string.
+    // Returns a boolean.
+    // Check if all parenthesis, curly-brackets, and square-brackets are balanced.
+    // Put into an array.
+    // Iterate through str.
+
+    for (let i = 0; i < str.length; i++) {
+
+        // If character is an opening brace add it to a stack
+        if (str[i] === '(' || str[i] === '{' || str[i] === '[' ) {
+            array.push(str[i]);
+        }
+        //  If that character is a closing brace, pop from the stack.
+        else {
+            let last = array.pop();
+
+            //  If the popped element from the stack doesn’t match the corresponding closing brace in the object, then return false.
+            if (str[i] !== correspond[last]) {return false};
+        }
+    }
+        // If the stack is not empty then false.
+        if (array.length !== 0) {return false};
+
+    return true;
+
+};
+
+
+
