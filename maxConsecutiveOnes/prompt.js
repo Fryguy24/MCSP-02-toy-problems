c815781ce50ba7dfb95330a7bfcca32e7eedecab
Julius Fry <fryjulius@gmail.com>
2020-01-09 10:03:50 -0600

    solution

--- maxConsecutiveOnes/prompt.js
+++ maxConsecutiveOnes/prompt.js
@@ -2,6 +2,23 @@
 
 //maxConsecutiveOnes([1,1,2,0,3,1,1,1,1,5,6,3]) returns 4
 
+
+// Return numbers that increase by one consecutively.
+// Output is the total amount of consecutive numbers.
+// Iterate through array.
+// Check if the numbers in array are consecutive.
+// 
 function maxConsecutiveOnes(array) {
-  //write solution code
-}
+  let max = 0;
+  let count = 0;
+  let start = array[0];
+  let arrLength = array.length;
+  
+    for (let i = 0; i < arrLength - 1; ++i) {
+      if (start + 1 ) {
+         start + 1;
+      }
+    }
+    return max;
+  }
+   //write solution code
